347d5e6f40d0035c16dc5baf460ccde21354a304
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-03 14:57:16 -0400

    added jwt support

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -2,6 +2,8 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+let jwt = require('jsonwebtoken');
+
 // create a reference to the model
 let Book = require('../models/book');
 
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,10 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let passport = require('passport');
 
+// enable jwt
+let jwt = require('jsonwebtoken');
+let DB = require('../config/db');
+
 // create the User Model instance
 let userModel = require('../models/user');
 let User = userModel.User; // alias
@@ -64,6 +68,28 @@ module.exports.processLoginPage = (req, res, next) => {
             {
                 return next(err);
             }
+
+            const payload = 
+            {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }
+
+            const authToken = jwt.sign(payload, DB.Secret, {
+                expiresIn: 604800 // 1 week
+            });
+
+            /* TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Logged in Successfully!', user: {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }, token: authToken});
+            */
+
             return res.redirect('/book-list');
         });
     })(req, res, next);
@@ -119,6 +145,10 @@ module.exports.processRegisterPage = (req, res, next) => {
             // if no error exists, then registration is successful
 
             // redirect the user and authenticate them
+
+            /* TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Registered Successfully!'});
+            */
 
             return passport.authenticate('local')(req, res, () => {
                 res.redirect('/book-list')
