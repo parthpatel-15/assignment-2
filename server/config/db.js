0fa237d15e883480becaf112928a8694be202777
parthpatel <parthpatel5333@gamil.com>
2021-10-26 03:47:19 +0530

    updated db uri to work online

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
     
-    "URI": "mongodb://localhost/my_express_site"
+    //"URI": "mongodb://localhost/my_express_site"
+    "URI": "mongodb+srv://Parth_15:P@rthp@tel15@dbserver.qp9c9.mongodb.net/my_business_contects?retryWrites=true&w=majority"
 }
